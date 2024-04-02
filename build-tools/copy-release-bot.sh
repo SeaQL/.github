2e4299ab5b86ec50fa687f0c8370a7e1f8e06426
Billy Chan <ccw.billy.123@gmail.com>
2024-04-02 13:00:24 +0800

    Release Bot

--- build-tools/copy-release-bot.sh
+++ build-tools/copy-release-bot.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -e
 if [ -d ./build-tools ]; then
-    targets=(
-        "sea-orm"
-        "sea-query"
-        "sea-schema"
-        "starfish-ql"
-        "seaography"
-        "sea-streamer"
-    )
-
-    for target in "${targets[@]}"; do
-        cp -TRv .github/workflows/release-bot.yml "../${target}/.github/workflows/release-bot.yml"
-        echo ""
-    done
+    find ../ -maxdepth 4 -type f -name 'release-bot.yml' -exec echo "" \; -exec echo {} \; -exec cp -TRv .github/workflows/release-bot-template.yml {} \;
 else
     echo "Please execute this script from the repository root."
 fi
